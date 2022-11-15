46548db97eab87ad34f3460a27fabbc794d603da
S. M. Redwan <smredwan.info@gmail.com>
2022-11-16 00:04:43 +0600

    Commited after complete the programe or game

--- tic_tac_toe/main.c
+++ tic_tac_toe/main.c
@@ -6,26 +6,46 @@
 char board[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 void printBoard();
+int check_win();
+void system();
 
-int main() {
+void main() {
     printBoard();
-    int player = 1, input;
+    int player = 1, input, status, result;
 
-    char mark = (player == 1) ? 'X' : 'O';
+    while(status = -1) {
+        player = (player % 2 == 0) ? 2 : 1;
+        char mark = (player == 1) ? 'X' : 'O';
 
-    scanf("%d", &input);
-    if(input < 1 || input > 9) {
-        printf("Invalid Input");
-    }   
+        up:
+        printf("Enter number for player1 %d: \n", player);
+        scanf("%d", &input);
+        if(input < 1 || input > 9) {
+            printf("Invalid Input\n");
+            goto up;
+        }
 
-    board[input] = mark;
+        board[input] = mark;
+        printBoard();
 
-    printBoard();
+        result = check_win();
+        // printf("%d", result);
 
-    return 0;
+        if(result == 1) {
+            printf("player %d !!!WINNER!!!\n", player);
+            return;
+        }
+        else if(result == 0) {
+            printf("!!!DRAW!!!\n");
+            return;
+        }
+
+        player++;
+    }
 }
 
 void printBoard() {
+    system("cls");
     printf("\n\n");
     printf("===TIC TAC TOE===\n\n");
     printf("     |     |     \n");
@@ -38,4 +58,44 @@ void printBoard() {
     printf("  %c  |  %c  |  %c  \n", board[7], board[8], board[9]);
     printf("     |     |     \n");
     printf("\n\n");
+}
+
+int check_win() {
+    if(board[1] == board[2] && board[2] == board[3]) {
+        return 1;
+    }
+    if(board[4] == board[5] && board[5] == board[6]) {
+        return 1;
+    }
+    if(board[7] == board[8] && board[8] == board[9]) {
+        return 1;
+    }
+    if(board[1] == board[4] && board[4] == board[7]) {
+        return 1;
+    }
+    if(board[2] == board[5] && board[5] == board[8]) {
+        return 1;
+    }
+    if(board[3] == board[6] && board[5] == board[9]) {
+        return 1;
+    }
+    if(board[3] == board[5] && board[5] == board[7]) {
+        return 1;
+    }
+    if(board[1] == board[5] && board[5] == board[9]) {
+        return 1;
+    }
+
+    int count = 0;
+    for(int i = 1; i <= 9; i++) {
+        if(board[i] == 'X' || board[i] == 'O') {
+            count++;
+        }
+    }
+
+    if(count == 9) {
+        return 0;
+    }
+    
+    return -1;
 }
