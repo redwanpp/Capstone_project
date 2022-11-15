783a703570afa28561f807b27fc77dccdb5508be
S. M. Redwan <smredwan.info@gmail.com>
2022-11-15 19:12:42 +0600

    First commit of this project. just print board

---
+++ tic_tac_toe/main.c
@@ -0,0 +1,26 @@
+//tic_tac_toe
+
+#include <stdio.h>
+#include <conio.h>
+
+void printBoard();
+
+int main() {
+    printBoard();
+
+    return 0;
+}
+
+void printBoard() {
+    printf("===TIC TAC TOE===\n\n");
+    printf("     |     |     \n");
+    printf("  1  |  2  |  3  \n");
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  4  |  5  |  6  \n");
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+    printf("  7  |  8  |  9  \n");
+    printf("     |     |     \n");
+    printf("\n\n");
+}
